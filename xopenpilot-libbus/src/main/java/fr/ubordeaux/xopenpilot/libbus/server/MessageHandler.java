2fe6210bf345da12cc1a04da50816cd1e67d8e68
Laetitia Leoni <laetitia.leoni@etu.u-bordeaux.fr>
2017-03-31 16:11:56 +0200

    maj MessageHandler, list a finir

--- xopenpilot-libbus/src/main/java/fr/ubordeaux/xopenpilot/libbus/server/MessageHandler.java
+++ xopenpilot-libbus/src/main/java/fr/ubordeaux/xopenpilot/libbus/server/MessageHandler.java
@@ -4,12 +4,22 @@ import java.io.StringReader;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.JsonValue;
 
 public class MessageHandler implements Server.MessageHandler {
 	private Bus bus;
 
 	public MessageHandler(Bus bus){
 		this.bus = bus;
+	}
+	/*a faire, une methode pour ack, JsonObject , cas d'erreur possible*/
+	
+	public String getStringIfPresent(JsonObject messageObject, String key){
+		JsonValue value = messageObject.get(key);
+		if(value != null)
+			return ((JsonString) value).getString();
+		return null;
 	}
 	
 	public String register(JsonObject messageObject){
@@ -23,6 +33,60 @@ public class MessageHandler implements Server.MessageHandler {
 		return response.toString();
 	}
 	
+	public String deregister(JsonObject messageObject){
+		this.bus.deregisterSender(messageObject.getInt("sender_id"));
+		JsonObject response = Json.createObjectBuilder()
+				.add("type", "deregister")
+				.add("ack", Json.createObjectBuilder()
+						.add("resp", "ok"))
+				.build();
+		return response.toString();
+	}
+	
+	public String list(JsonObject messageObject){
+		String sender_class = getStringIfPresent(messageObject, "sender_class");
+		String sender_name = getStringIfPresent(messageObject, "sender_name");
+		
+		/*a finir, create arrayBuilder*/
+		return null;
+	}
+	
+	public String send(JsonObject messageObject){
+		this.bus.messageReceived(messageObject.getInt("sender_id"), messageObject.get("contents"));
+		JsonObject response = Json.createObjectBuilder()
+				.add("type", "send")
+				.add("ack", Json.createObjectBuilder()
+						.add("resp", "ok"))
+				.build();
+		return response.toString();
+	}
+	
+	public String get(JsonObject messageObject){
+		MessageServer messageServer = this.bus.getMessage(messageObject.getInt("sender_id"), messageObject.getInt("msg_id"));
+		JsonObject response = Json.createObjectBuilder()
+				.add("type", "get")
+				.add("ack", Json.createObjectBuilder()
+						.add("resp", "ok"))
+				.add("msg_id", messageServer.getId())
+				.add("date", messageServer.getDate().getTime())
+				.add("contents", messageServer.getContent())
+				.build();
+		return response.toString();
+	}
+	
+	public String getLast(JsonObject messageObject){
+		MessageServer messageServer = this.bus.getLastMessage(messageObject.getInt("sender_id"));
+		JsonObject response = Json.createObjectBuilder()
+				.add("type", "get_last")
+				.add("ack", Json.createObjectBuilder()
+						.add("resp", "ok"))
+				.add("msg_id", messageServer.getId())
+				.add("date", messageServer.getDate().getTime())
+				.add("contents", messageServer.getContent())
+				.build();
+		return response.toString();
+	}
+	
 	@Override
 	public String messageReceived(String messageLine) {
 		String type;
@@ -31,6 +95,16 @@ public class MessageHandler implements Server.MessageHandler {
 	    type = messageObject.getString("type");
 		if (type == "register")
 			return register(messageObject);
+		if (type == "deregister")
+			return deregister(messageObject);
+		if(type == "list")
+			return list(messageObject);
+		if(type == "send")
+			return send(messageObject);
+		if(type == "get")
+			return get(messageObject);
+		if(type == "get_last")
+			return getLast(messageObject);
 		/*if type == etc etc return methode*/
 			
 		return null;
