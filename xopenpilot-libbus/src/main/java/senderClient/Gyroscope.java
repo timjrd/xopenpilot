3fd8f35d3105b09aef88100dbdf39468b04828d3
Laetitia Leoni <laetitia.leoni@etu.u-bordeaux.fr>
2017-04-14 16:59:05 +0200

    correction du Gyroscope

--- xopenpilot-libbus/src/main/java/senderClient/Gyroscope.java
+++ xopenpilot-libbus/src/main/java/senderClient/Gyroscope.java
@@ -12,6 +12,8 @@ import fr.ubordeaux.xopenpilot.libbus.client.Sender;
 public class Gyroscope {
 	private JsonObject contents;
 	private double x,y,z;
+	private RemoteBus bus;
+	private Sender sender; 
 
 	public JsonObject getContents() {
 		return contents;
@@ -45,30 +47,53 @@ public class Gyroscope {
 		this.z = z;
 	}
 	
-	public Gyroscope() throws IOException{
-		RemoteBus bus = new RemoteBus("localhost", 7182);
-		Sender sender = bus.registerSender("Gyroscope", "monGyroscope");
-		Scanner in = new Scanner(System.in);
+	public Gyroscope(String host) throws IOException{
+		this.bus = new RemoteBus(host, 7182);
+		this.sender = bus.registerSender("Gyroscope", "monGyroscope");
 		
+	}
+	
+	public void run() throws IOException{
+		boolean running = true; 
+		String line = "";
+		Scanner in = new Scanner(System.in);
 		if (sender != null){
-			while (true){
-				System.out.println("<enter message>");
-				String line = in.nextLine();
-				x = Double.parseDouble(line);
-				line = in.nextLine();
-				y = Double.parseDouble(line);
-				line = in.nextLine();
-				z = Double.parseDouble(line);				
-
-				contents = Json.createObjectBuilder()
-						.add("x", x)
-						.add("y", y)
-						.add("z", z)
-						.build();
-				
-				sender.sendMessage(contents);
+			while (running){
+				System.out.println("<send position x y z>");	
+				line = in.nextLine();		
+				if(line.equals("quit")){
+					running = false;
+				}			
+				else{
+					x = Double.parseDouble(line);
+					line = in.nextLine();
+					y = Double.parseDouble(line);
+					line = in.nextLine();
+					z = Double.parseDouble(line);				
+		
+					contents = Json.createObjectBuilder()
+							.add("x", x)
+							.add("y", y)
+							.add("z", z)
+							.build();
+					
+					sender.sendMessage(contents);
+				}
 			}
 		}
+	}
+	
+	public void close() throws IOException{
+		if (sender != null){
+			sender.deregister();
+			bus.close();
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		Gyroscope gyro = new Gyroscope("10.0.104.10");
+		gyro.run();
+		gyro.close();
 	}
 	
 }
